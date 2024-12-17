a5b98f9ab3808be63d2c2965c806ffe4431decd4
Rushikesh-Yeole <rushisyeole@gmail.com>
2024-12-17 20:10:44 +0530

    connect

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContextProvider=(props)=> {
 
     axios.defaults.withCredentials = true;
     
-    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const backendUrl="https://kiwi-pzod.onrender.com" || VITE_BACKEND_URL
     const [isLoggedIn, setIsLoggedIn]=useState(false)
     const [userData, setUserData]=useState(null)
     
